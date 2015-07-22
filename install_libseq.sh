a76587ef10c22239280f1aad8820f03c849bb40b
Kevin R. Thornton <krthornt@uci.edu>
2015-07-22 11:38:29 -0700

    try to install into home

--- install_libseq.sh
+++ install_libseq.sh
@@ -4,4 +4,4 @@
 wget http://github.com/molpopgen/libsequence/archive/1.8.5.tar.gz
 tar xzf 1.8.5.tar.gz
 cd libsequence-1.8.5
-./configure && make install
+./configure --prefix=$HOME && make install
